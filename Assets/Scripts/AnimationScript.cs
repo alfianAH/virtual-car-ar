ca513fb05bda8ae1f1476d129e155c95e3f9f92b
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-11-27 15:51:14 +0800

    Add smash animation controller through script and button

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationScript : MonoBehaviour
+{
+   
+    [SerializeField] private GameObject replayArea;
+
+    private Animator carAnimator;
+
+    private void Start()
+    {
+        carAnimator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        StartCoroutine(Delay(3f));
+    }
+
+    public void ReplayScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void ShowReplayArea()
+    {
+        replayArea.SetActive(true);
+    }
+
+    private IEnumerator Delay(float second)
+    {
+        yield return new WaitForSeconds(second);
+        carAnimator.SetTrigger("PlayAnim");
+    }
+}
